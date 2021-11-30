ca11469a0b03e50d654a1e3bf091520724214412
Mrh Ripon <mrhripon98@gmail.com>
2021-11-30 19:56:27 +0600

    footer and loader fixed

--- public/js/all.js
+++ public/js/all.js
@@ -2,17 +2,17 @@ $('#overlay').hide();
 setTimeout(() => {
     $('#overlay').hide();
     $('#container').show();
-},7000);
+}, 3000);
 $('#overlay').show();
 $('#container').hide()
 function Link(val) {
     //alert(val)
-    for(let i = 1; i <= 4; i++ ) {
-    if(val == i) {
-        document.getElementById(`tag${i}`).style.borderBottom = '5px solid #4598ff';
-    } else {
-        document.getElementById(`tag${i}`).style.borderBottom = 'none';       
-    } 
-}
+    for (let i = 1; i <= 4; i++) {
+        if (val == i) {
+            document.getElementById(`tag${i}`).style.borderBottom = '5px solid #4598ff';
+        } else {
+            document.getElementById(`tag${i}`).style.borderBottom = 'none';
+        }
+    }
 
 }
